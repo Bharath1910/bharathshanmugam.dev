aa775d47c79b96dd60c4ac79e6f7f8ecbc753bb2
Bharath1910 <av.bharath@proton.me>
2023-03-03 15:31:41 +0530

    background completed

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,23 @@ function Home() {
                 <div className='absolute left-24 top-24 rotate-45 w-56 h-72 bg-complement rounded-full p-5'></div>
             </div>
             <div className="bg-background h-screen flex">
-                <div className="flex-1 text-xl text-white flex flex-col justify-center gap-4 p-16 z-10">
-                    <h1 className='font-poppins'>Hello! I'm Bharath Shanmugam AV</h1>
-                    <p className='text-8xl font-gloock'>Full stack web developer from India</p>
-                    <p className='font-poppins'>I specilise in the MERN tech stack, Powered by VSCode ;)</p>
+                <div className="flex-1 text-xl text-white flex justify-center items-center p-16 z-10">
+                    <div className='flex flex-col gap-4'>
+                        <h1 className='font-poppins'>Hello! I'm Bharath Shanmugam AV</h1>
+                        <p className='text-8xl font-gloock'>Full stack web developer from India</p>
+                        <p className='font-poppins'>I specilise in the MERN tech stack, Powered by VSCode ;)</p>
 
-                    <div className='flex mt-6'>
-                        <a className='bg-complement text-background font-poppins px-10 py-4 rounded-lg' href='https://google.com' target={"_blank"}>Github</a>
-                        <a className='text-complement font-poppins px-10 py-4' href='https://google.com' target={"_blank"}>Email</a>
+                        <div className='flex mt-6'>
+                            <a className='bg-complement text-background font-poppins px-10 py-4 rounded-lg' href='https://google.com' target={"_blank"}>Github</a>
+                            <a className='text-complement font-poppins px-10 py-4' href='https://google.com' target={"_blank"}>Email</a>
+                        </div>
                     </div>
                 </div>
 
-                <div className="flex-1">
-                    <Image src="/hero_background_bg_removed.png" width={533} height={800} />
+                <div className="flex-1 flex justify-center items-center">
+                    <div>
+                        <img className='aspect-hero w-96' src="/hero_background_bg_removed.png" />
+                    </div>
                 </div>
             </div>
         </>
