588fd4fd9c7cdc5774fb2e9523798759e5e3bfdc
Bharath1910 <av.bharath@proton.me>
2023-06-04 13:24:16 +0530

    fixed some bugs

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <>
             <div className='blur-4xl'>
-                <div className='absolute left-24 top-20 rotate-45 w-56 h-72 bg-blue-dark rounded-full p-5'></div>
+                <div className='overflow-clip absolute left-10 sm:left-24 top-20 rotate-45 w-52 h-52 sm:w-56 sm:h-72 bg-blue-dark rounded-full p-5'></div>
             </div>
 
             <div className="bg-background h-[80vh] 2xl:h-[90vh] flex">
@@ -14,7 +14,7 @@ function Hero() {
                             <p className="p-1 px-2 font-raleway">Hello! 👋</p>
                             <h1 className='font-raleway p-1 px-2 rounded-md w-fit'><span className=''>I'm Bharath Shanmugam AV</span></h1>
                         </div>
-                        <p className='text-[70px] md:text-7xl 2xl:text-8xl leading-none font-raleway font-extrabold'><span className="underline decoration-pink decoration-8">I'm a Backend</span> developer, from <span className={styles.grad}>India.</span></p>
+                        <p className='text-[60px] md:text-7xl 2xl:text-8xl leading-none font-raleway font-extrabold'><span className="underline decoration-pink decoration-8">I'm a Backend</span> developer, from <span className={styles.grad}>India.</span></p>
                         <div className='flex mt-6 justify-start'>
                             <a className={styles.btn} href='https://github.com/bharath1910' target={"_blank"}>Github</a>
                             <a className="text-complement font-raleway px-10 py-4" href='mailto:av.bharath@proton.me' target={"_blank"}>Email</a>
