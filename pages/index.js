e02819d19e46f1e62c60e8633f78b25e70636f37
Bharath1910 <av.bharath@proton.me>
2023-06-04 10:53:05 +0530

    fixed import error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import About from '../components/About'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
-import Skills from '../components/Skills'
 
 function Home() {
   return (
