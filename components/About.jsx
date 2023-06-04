086771497771c62bbf7737ce4bbb5dc3a170fe1e
Bharath1910 <av.bharath@proton.me>
2023-06-04 10:37:52 +0530

    about fully responsive

--- components/About.jsx
+++ components/About.jsx
@@ -3,11 +3,11 @@ import styles from  '../styles/About.module.css'
 function About() {
   return (
     <div>
-        <div className="bg-background h-fit flex sm:flex-col-reverse xl:flex-row-reverse xl:pt-24">
-            <div className="flex-1 text-xl text-white flex justify-center items-center p-16 xl:pl-28 z-10">
+        <div className="bg-background h-fit flex flex-col-reverse xl:flex-row-reverse xl:pt-24">
+            <div className="flex-1 text-xl text-white flex justify-center items-center px-4 md:px-24 xl:px-2 2xl:px-20 z-10">
                 <div className='flex flex-col gap-6'>
-                    <p className='text-8xl font-oswald'>About <span className=''>Me</span></p>
-                    <p className='font-montserrat'>
+                    <p className='text-7xl lg:text-8xl font-oswald'>About <span className=''>Me</span></p>
+                    <p className='text-base lg:text-xl font-montserrat'>
                         Hello! My name is A.V.Bharath Shanmugam, from India. 
                         I love creating stuffs which runs on the web. 
                         I started my web development journey a month ago,
@@ -28,23 +28,10 @@ function About() {
                 </div>
             </div>
                 
-            <div className="p-24 flex justify-center sm:items-center sm:w-auto sm:h-auto">
-
-                <div className='relative'>
-                    <div className='z-20 relative w-fit'>
-                        <img className='w-96' src="/me.png" />
-                    </div>
-
-                    <div className='absolute top-0 -rotate-12 z-10'>
-                        <div className='w-96 h-96 border-solid rounded-xl border-2 border-white'></div>
-                    </div>
-
-                    <div className='z-0 w-96 h-96 absolute top-0 blur-4xl'>
-                        <div className={styles.triPink}></div>
-                    </div>
-
-                    <div className='z-0 w-96 h-96 absolute blur-4xl -top-20'>
-                        <div className={styles.tri}></div>
+            <div className="p-6 md:p-20 flex justify-start sm:items-center sm:w-auto sm:h-auto">
+                <div className=''>
+                    <div className='w-fit md:w-[500px] 2xl:w-[600px]'>
+                        <img className='rounded-3xl' src="/me.png" />
                     </div>
                 </div>
             </div>
