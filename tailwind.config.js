72d07dd3660d1296f49b9482177ab56fa1c095cc
Bharath1910 <av.bharath@proton.me>
2024-02-25 23:17:06 +0530

    completed mobile nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'background': '#12161c',
+      'accent': '#3FFFB2',
+      'complement': '#3EECFF',
+      'pink': '#FF3D8B',
+      'white': '#FFFFFF',
+      'blue-dark': '#25becf',
+    },
     extend: {
       fontFamily: {
-        'satisfy': ['Satisfy', 'cursive']
+        'satisfy': ['Satisfy', 'cursive'],
+        'raleway': ['Raleway', 'Sans serif'],
+        'poppins': ['Poppins', 'Sans serif']
       }
     },
   },
